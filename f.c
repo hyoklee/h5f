7d492a78a7fc0a73810ac57ccc32a24b12a3fe92
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-04-29 22:15:06 -0500

    fix: return codes

--- f.c
+++ f.c
@@ -2,9 +2,24 @@
 
 #include <fuse.h>
 
-main()
+int h5f_close(int fid)
 {
   return 0;
+}
+
+int h5f_create(char* name)
+{
+  return 0;
+}
+
+int main()
+{
+  int fid;
+  char* name = "t.fs";
+  
+  fid = h5f_create(name);       /* call mkfs open */
+  return h5f_close(fid);        /* call mkfs close */
+
 }
   
 
